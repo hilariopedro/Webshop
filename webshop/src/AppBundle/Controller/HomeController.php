f6e0f3ec81164699d7289f28b58eaaac12516a79
Jelle <jelle.streekstra@hotmail.com>
2016-06-03 10:37:59 +0200

    Adds all controllers and views

---
+++ webshop/src/AppBundle/Controller/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jelle
+ * Date: 2-6-2016
+ * Time: 12:54
+ */
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('home/index.html.twig');
+    }
+}
---
+++ webshop/src/AppBundle/Controller/PortfolioController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jelle
+ * Date: 2-6-2016
+ * Time: 12:55
+ */
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class PortfolioController extends Controller
+{
+
+}
